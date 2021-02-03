e753691833dee529828d48778f44bf92188258e4
Leon Woestenberg <leon@sidebranch.com>
2021-02-03 15:04:03 +0100

    Add block diagram wrapper (not sure if can be generated).
    Signed-off-by: Leon Woestenberg <leon@sidebranch.com>

---
+++ amd/bd/processing_system/hdl/processing_system_wrapper.v
@@ -0,0 +1,18 @@
+//Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2019.1 (lin64) Build 2552052 Fri May 24 14:47:09 MDT 2019
+//Date        : Sat Jan 23 23:18:04 2021
+//Host        : lunar running 64-bit Linux Mint 19.2 Tina
+//Command     : generate_target processing_system_wrapper.bd
+//Design      : processing_system_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module processing_system_wrapper
+   ();
+
+
+  processing_system processing_system_i
+       ();
+endmodule
